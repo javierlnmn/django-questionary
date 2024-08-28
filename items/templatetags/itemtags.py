3305253a498a7ef5f83ecefac53eb6d13f829253
javierlnmn <jlm03123@gmail.com>
2024-08-28 12:19:43 +0200

    In course quizzes tracking

--- items/templatetags/itemtags.py
+++ items/templatetags/itemtags.py
@@ -16,3 +16,12 @@ def is_quiz_completed_by_user(quiz, user):
 def is_quiz_passed_by_user(quiz, user):
     quiz_entry = QuizEntry.objects.filter(quiz=quiz, respondent=user).first()
     return quiz_entry.is_passed
+
+@register.simple_tag
+def remaining_items_by_user(quiz, user):
+    quiz_entry = QuizEntry.objects.filter(quiz=quiz, respondent=user).first()
+    if not quiz_entry: 
+        return 0
+    quiz_items = quiz_entry.quiz.items.count()
+    answered_items = quiz_entry.answers.count()
+    return 0 if quiz_items == answered_items or answered_items == 0 else quiz_items - answered_items
